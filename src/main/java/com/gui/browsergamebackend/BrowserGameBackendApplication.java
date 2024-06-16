387e2095edfecdf38b219149dc7272d57d19cf3f
GuillaumeLambermont <guillaume.lambermont@gmail.com>
2024-06-16 13:00:14 +0200

    Initial commit

---
+++ src/main/java/com/gui/browsergamebackend/BrowserGameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gui.browsergamebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrowserGameBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrowserGameBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gui/browsergamebackend/BrowserGameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gui.browsergamebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrowserGameBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
